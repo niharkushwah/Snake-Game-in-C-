95cb2a64fab1ddd214a8f622f587dc5eb84ef749
Nihar Kushwah <59207039+niharkushwah@users.noreply.github.com>
2020-11-19 11:19:05 +0530

    Add files via upload

---
+++ snack.cpp
@@ -0,0 +1,169 @@
+/*
+NIHAR KUSHWAH
+*/
+
+#include <iostream>
+#include <windows.h>
+#include <conio.h>
+#define ARROW_UP 72
+#define ARROW_DOWN 80
+#define ARROW_LEFT 75
+#define ARROW_RIGHT 77
+using namespace std;
+bool gameOver;
+const int width = 20;
+const int height = 20;
+int x, y, fruitX, fruitY, score;
+int tailX[100], tailY[100];
+int nTail;
+enum eDirecton { STOP = 0, LEFT, RIGHT, UP, DOWN};
+eDirecton dir;
+class snake1 {
+    public:
+void Setup()
+{
+    gameOver = false;
+    dir = STOP;
+    x = width / 2;
+    y = height / 2;
+    fruitX = rand() % width;
+    fruitY = rand() % height;
+    score = 0;
+}};
+class snake2:public snake1 {
+public:
+void Draw()
+{
+    system("cls");
+    for (int i = 0; i < width+2; i++) //top border
+        cout << "#";
+    cout << endl;
+
+    for (int i = 0; i < height; i++)
+    {
+        for (int j = 0; j < width; j++)
+        {
+            if (j == 0)
+                cout << "#";
+            if (i == y && j == x)
+                cout << "O";
+            else if (i == fruitY && j == fruitX)
+                cout << "F";
+            else
+            {
+                bool print = false;
+                for (int k = 0; k < nTail; k++)
+                {
+                    if (tailX[k] == j && tailY[k] == i)
+                    {
+                        cout << "*";
+
+                        print = true;
+                    }
+                }
+                if (!print)
+                    cout << " ";
+            }
+
+
+            if (j == width - 1)
+                cout << "#";
+        }
+        cout << endl;
+    }
+
+    for (int i = 0; i < width+2; i++) // bottom border
+        cout << "#";
+    cout << endl;
+    cout << "Score:" << score << endl;
+}};
+class snake3 : public snake2
+{
+public:
+void Input()
+{
+    if (_kbhit())
+    {
+        switch (_getch()) //ascii value
+        {
+        case 75 :
+            dir = LEFT;
+            break;
+        case 77:
+            dir = RIGHT;
+            break;
+        case 72:
+            dir = UP;
+            break;
+        case 80:
+            dir = DOWN;
+            break;
+        case 'x':
+            gameOver = true;
+            break;
+        }
+    }
+}
+void Logic()
+{
+    int prevX = tailX[0];
+    int prevY = tailY[0];
+    int prev2X, prev2Y;
+    tailX[0] = x;
+    tailY[0] = y;
+    for (int i = 1; i < nTail; i++)
+    {
+        prev2X = tailX[i];
+        prev2Y = tailY[i];
+        tailX[i] = prevX;
+        tailY[i] = prevY;
+        prevX = prev2X;
+        prevY = prev2Y;
+    }
+    switch (dir)
+    {
+    case LEFT:
+        x--;
+        break;
+    case RIGHT:
+        x++;
+        break;
+    case UP:
+        y--;
+        break;
+    case DOWN:
+        y++;
+        break;
+    default:
+        break;
+    }
+
+    if (x >= width) x = 0; else if (x < 0) x = width - 1;
+    if (y >= height) y = 0; else if (y < 0) y = height - 1;
+
+    for (int i = 0; i < nTail; i++)
+        if (tailX[i] == x && tailY[i] == y)
+            gameOver = true;
+
+    if (x == fruitX && y == fruitY)
+    {
+        score += 10;
+        fruitX = rand() % width;
+        fruitY = rand() % height;
+        nTail++;
+    }
+}};
+
+int main()
+{
+    snake3 obj1;
+    obj1.Setup();
+    while ( ! gameOver)
+    {
+        obj1.Draw();
+        obj1.Input();
+        obj1.Logic();
+        Sleep(200);
+    }
+    return 0;
+}
